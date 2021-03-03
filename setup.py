eb369ee10fc3d6fd31cefc35fffbb800c4b77187
xju <xju@cern.ch>
2021-03-03 11:16:46 -0800

    starting point

---
+++ setup.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+description="Use GAN to generate particle physics events"
+
+setup(
+    name="gan4hep",
+    version="0.1.0",
+    description=description,
+    long_description=description,
+    author="Xiangyang Ju",
+    license="Apache License, Version 2.0",
+    keywords=["GAN", "HEP"],
+    url="https://github.com/xju2/gan4hep",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'tensorflow >= 2.4.0',
+        "matplotlib",
+        'tqdm',
+    ],
+    # package_data = {
+    #     "gan4hep": ["config/*.yaml"]
+    # },
+    setup_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+    scripts=[
+        'gan4hep/scripts/train_gan.py',
+        'gan4hep/scripts/train_mlp_gan.py',
+        'gan4hep/scripts/train_rnn_mlp_gan.py',
+    ],
+)
