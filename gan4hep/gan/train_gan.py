1b35c9aed55caa1a9350832ceeb7840636d3774e
PeterFitzhugh <92994722+PeterFitzhugh@users.noreply.github.com>
2021-12-13 09:53:23 +0100

    Update train_gan.py

--- gan4hep/gan/train_gan.py
+++ gan4hep/gan/train_gan.py
@@ -12,10 +12,11 @@ from wgan import WGAN
 all_gans = ['GAN', "AAE", 'CGAN', 'WGAN']
 
 from gan4hep.utils_gan import generate_and_save_images
+from gan4hep.utils_gan import generate_and_save_images_end_of_run
 from gan4hep.preprocess import herwig_angles
 from gan4hep.preprocess import dimuon_inclusive
 
-def inference(gan, test_in, test_truth, log_dir):
+def inference(gan, test_in, test_truth, log_dir): 
     checkpoint_dir = os.path.join(log_dir, "checkpoints")
     checkpoint = tf.train.Checkpoint(
         generator=gan.generator,
@@ -41,7 +42,11 @@ def inference(gan, test_in, test_truth, log_dir):
     print(tot_wdis)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #Code only runs if its called by the terminal and not by another .py file
+    
+    
+    # How to alter the parameters of the GAN in the CMD line terminal
+    
     import argparse
     parser = argparse.ArgumentParser(description='Train The GAN')
     add_arg = parser.add_argument
@@ -53,35 +58,57 @@ if __name__ == '__main__':
     add_arg("--inference", help='perform inference only', action='store_true')
     add_arg("-v", '--verbose', help='tf logging verbosity', default='INFO',
         choices=['WARN', 'INFO', "ERROR", "FATAL", 'DEBUG'])
-    add_arg("--max-evts", help='Maximum number of events', type=int, default=None)
+    add_arg("--max-evts", help='Maximum number of events', type=int, default=10000)
     add_arg("--batch-size", help='Batch size', type=int, default=512)
     add_arg("--lr", help='learning rate', type=float, default=0.0001)
+    add_arg("--test-frac", help='Fraction of data used for testing', type=float, default=0.1)
     add_arg("--data", default='herwig_angles',
         choices=['herwig_angles', 'dimuon_inclusive'])
 
     # model parameters
+    add_arg("--gen-layers", type=int, default=0, help='Number of extra layers to add to generator model with 256 nodes')
+    add_arg("--dis-layers", type=int, default=0, help='Number of extra layers to add to discriminator model with 256 nodes')
     add_arg("--noise-dim", type=int, default=4, help="noise dimension")
+    add_arg("--num-nodes", type=int, default=256, help="Number of Nodes in a NN Layer")
     add_arg("--gen-output-dim", type=int, default=2, help='generator output dimension')
     add_arg("--cond-dim", type=int, default=0, help='dimension of conditional input')
     add_arg("--disable-tqdm", action="store_true", help='disable tqdm')
-
+    add_arg("--noise-type", default='gaussian',
+            choices=['gaussian','uniform'])
+    
+    #Creates namespace
+   
     args = parser.parse_args()
 
     from tensorflow.compat.v1 import logging
-    logging.set_verbosity(args.verbose)
+    logging.set_verbosity(args.verbose) 
 
     # prepare input data by calling those function implemented in 
     # gan4hep.preprocess.
     train_in, train_truth, test_in, test_truth = eval(args.data)(
-        args.filename, max_evts=args.max_evts)
+        args.filename, max_evts=args.max_evts,testing_frac=args.test_frac) #Split into train and test dataset with max number of events and finds out whether args.data is set to herwig_angles or dimuon_inclusive
 
-    batch_size = args.batch_size
-    gan = eval(args.model)(**vars(args))
+    batch_size = args.batch_size # Get input batch size
+    
+    from tensorflow import keras
+    from tensorflow.keras import layers
+    
+    gan = eval(args.model)(**vars(args)) # eval(args.model) FInds what type of NN has been picked in the input and find the           corresponding class
+    
+    #Define variables from args to be given to gan.train (These lines might be useless)
+    gan.lr=args.lr
+    lr=args.lr
+    gen_layers=args.gen_layers
+    dis_layers=args.dis_layers
+    noise_type=args.noise_type
+    num_nodes=args.num_nodes
+  
+
     if args.inference:
-        inference(gan, test_in, test_truth, args.log_dir)
+        inference(gan, test_in, test_truth, args.log_dir) # Run inference function at the top of the page
     else:
-        gan.train(
+        gan.train(args,
             train_truth, args.epochs, batch_size,
             test_truth, args.log_dir,
-            generate_and_save_images,
+            generate_and_save_images,generate_and_save_images_end_of_run,lr,noise_type,gen_layers,dis_layers,num_nodes,
             train_in, test_in)
