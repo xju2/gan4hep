5ce54904c9f013d1fe7c40db01884a8abcc9f284
PeterFitzhugh <92994722+PeterFitzhugh@users.noreply.github.com>
2022-02-04 11:33:03 +0100

    Update utils.py

--- gan4hep/nf/utils.py
+++ gan4hep/nf/utils.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 
-__all__= ["train_density_estimation", "nll"]
+__all__= [
+    "train_density_estimation",
+    "train_density_estimation_cond",
+    "nll"]
 @tf.function
 def train_density_estimation(distribution, optimizer, batch):
     """
@@ -13,6 +16,29 @@ def train_density_estimation(distribution, optimizer, batch):
     with tf.GradientTape() as tape:
         tape.watch(distribution.trainable_variables)
         loss = -tf.reduce_mean(distribution.log_prob(batch))  # negative log likelihood
+    gradients = tape.gradient(loss, distribution.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, distribution.trainable_variables))
+    return loss
+
+
+@tf.function
+def train_density_estimation_cond(distribution, optimizer, batch, condition, layers):
+    """
+    Train function for density estimation normalizing flows.
+    :param distribution: TensorFlow distribution, e.g. tf.TransformedDistribution.
+    :param optimizer: TensorFlow keras optimizer, e.g. tf.keras.optimizers.Adam(..)
+    :param batch: Batch of the train data.
+    :return: loss.
+    """
+    cond_kwargs = dict([(f"b{idx}", {"conditional_input": condition}) for idx in range(layers)])
+    with tf.GradientTape() as tape:
+        tape.watch(distribution.trainable_variables)
+        loss = -tf.reduce_mean(
+            distribution.log_prob(
+                batch, **cond_kwargs
+                # bijector_kwargs={'conditional_input': condition}
+                ))
+
     gradients = tape.gradient(loss, distribution.trainable_variables)
     optimizer.apply_gradients(zip(gradients, distribution.trainable_variables))
     return loss
