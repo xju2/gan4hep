e0716d33522eac31d16c3c2d2f5b1fc72ce9833b
PeterFitzhugh <92994722+PeterFitzhugh@users.noreply.github.com>
2022-03-01 15:09:40 +0100

    Add files via upload

--- gan4hep/preprocess.py
+++ gan4hep/preprocess.py
@@ -164,83 +164,14 @@ def herwig_angles2(filename,
 
     return (train_in, train_truth, test_in, test_truth, xlabels)
 
-def herwig_angles2(filename,
-        max_evts=None, testing_frac=0.1, mode=2):
-    """
-    This Herwig dataset is for the "ClusterDecayer" study.
-    Each event has q1, q1, cluster, h1, h2.
-    I define 3 modes:
-    0) both q1, q2 are with Pert=1
-    1) either q1 or q2 is with Pert=1
-    2) neither q1 nor q2 are with Pert=1
-    """
-    print(f'reading from {filename}')
-    df = read_dataframe(filename, ";", 'python')
 
-    def split_to_float(df, sep=','):
-        out = df
-        if type(df.iloc[0]) == str:
-            out = df.str.split(sep, expand=True).astype(np.float32)
-        return out
+def dimuon_inclusive(filename, max_evts=1000000, testing_frac=0.1):
 
-    q1,q2,c,h1,h2 = [split_to_float(df[idx]) for idx in range(5)]
-    if mode not in [0, 1, 2]:
-        mode = 2
-        print(f"mode {mode} is not known! use mode 2")
-
-    if mode == 0:
-        selections = (q1[5] == 1) & (q2[5] == 1)
-    elif mode == 1:
-        selections = ((q1[5] == 1) & (q2[5] == 0)) | ((q1[5] == 0) & (q2[5] == 1))
-    else:
-        selections = (q1[5] == 0) & (q2[5] == 0)
-        
-    # selection hadrons in one of the modes    
-    h1 = h1[selections]
-    h2 = h2[selections]
-
-    # ======================================
-    # Calculate the theta and phi angle, and the energy
-    # of the first outgoing particle
-    # ======================================
-    scaler = MinMaxScaler(feature_range=(-1,1))
-
-    out_4vec = h1.values
-    energy,px,py,pz = [out_4vec[:, idx] for idx in range(1,5)]
-    pT = np.sqrt(px**2 + py**2)
-    phi = np.arctan(px/py)
-    theta = np.arctan(pT/pz)
-    truth_in = np.stack([phi, theta, energy], axis=1)
-    truth_in = scaler.fit_transform(truth_in)
-    print("Min and Max for hadrons: ", scaler.data_min_, scaler.data_max_)
-
-    # the input 4vector is the cluster 4vector
-    input_4vec = c[[1, 2, 3, 4]][selections].values
-    input_4vec = scaler.fit_transform(input_4vec)
-    print("Min and Max for clusters: ", scaler.data_min_, scaler.data_max_)
-
-    shuffle(truth_in)
-    shuffle(input_4vec)
-    print(truth_in.shape, input_4vec.shape)
-
-
-    # Split the data into training and testing
-    # <HACK, FIXME, NOTE>
-    # <HACK, For now a maximum of 10,000 events are used for testing, xju>
-    num_test_evts = int(input_4vec.shape[0]*testing_frac)
-    if num_test_evts < 10_000: num_test_evts = 10_000
-
-    test_in, train_in = input_4vec[:num_test_evts], input_4vec[num_test_evts:max_evts]
-    test_truth, train_truth = truth_in[:num_test_evts], truth_in[num_test_evts:max_evts]
-
-    return (train_in, train_truth, test_in, test_truth)
-
-
-def dimuon_inclusive(filename, max_evts=None, testing_frac=0.1):
     
     df = read_dataframe(filename, " ", None)
     truth_data_1 = df.to_numpy().astype(np.float32)
     print(f"reading dimuon {df.shape[0]} events from file {filename}")
+    print('please print')
 
     scaler = MinMaxScaler(feature_range=(-1,1))
     truth_data = scaler.fit_transform(truth_data_1)
@@ -259,5 +190,5 @@ def dimuon_inclusive(filename, max_evts=None, testing_frac=0.1):
 
     xlabels = ['leading Muon {}'.format(name) for name in ['pT', 'eta', 'phi']] +\
               ['subleading Muon {}'.format(name) for name in ['pT', 'eta', 'phi']]
-    
+
     return (None, train_truth, None, test_truth, xlabels,test_truth_1,train_truth_1)
