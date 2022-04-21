90be9e2c7e97a8b1d7a0c11f1b8b0711ffdce1d9
PeterFitzhugh <92994722+PeterFitzhugh@users.noreply.github.com>
2022-04-21 15:20:02 +0200

    Update made.py

--- gan4hep/nf/made.py
+++ gan4hep/nf/made.py
@@ -1,23 +1,129 @@
+"""Masked Autoregressive Density Estimation
+"""
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+tfd = tfp.distributions
+tfb = tfp.bijectors
+tfk = tf.keras
+
+class Made(tfk.layers.Layer):
+    def __init__(self, params,
+        event_shape=None,
+        conditional=False,
+        conditional_event_shape=None,
+        hidden_units=None, activation=None,
+        use_bias=True, kernel_regularizer=None,
+        bias_regularizer=None, name="made") -> None:
+        super().__init__(name=name)
+
+        self.params = params
+        self.event_shape = event_shape
+        self.use_conditional = conditional
+        self.hidden_units = hidden_units
+        self.activation = activation
+        self.use_bias = use_bias
+        self.kernel_regularizer = kernel_regularizer
+        self.bias_regularizer = bias_regularizer
+
+        self.network = tfb.AutoregressiveNetwork(
+            params=params,
+            event_shape=event_shape,
+            conditional=conditional,
+            conditional_event_shape=conditional_event_shape,
+            hidden_units=hidden_units,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer)
+
+    def call(self, x, conditional_input=None):
+        if self.use_conditional:
+            out = self.network(x, conditional_input=conditional_input)
+        else:
+            out = self.network(x)
+        shift, log_scale = tf.unstack(out, num=2, axis=-1)
+        return shift, tf.math.tanh(log_scale)
+
+
+def create_flow(hidden_shape: list, layers: int, input_dim: int, with_condition: bool =False):
+    """Create Masked Autogressive Flow for density estimation
+    Arguments:
+    hidden_shape -- Multilayer Perceptron shape
+    layers -- Number of bijectors
     """
-    The primary training loop
+
+
+    print('hidden shape',hidden_shape)
+    print('layers',layers)
+    print('input dim',input_dim)
+
+    base_dist = tfd.Normal(loc=0.0, scale=1.0)
+    permutation = tf.cast(np.concatenate((
+        np.arange(input_dim / 2, input_dim), np.arange(0, input_dim / 2))), tf.int32)
+    bijectors = []
+    if (input_dim % 2) == 0:
+        permutation = permutation
+    else:
+        permutation = permutation[:-1]
+    print('permutation',permutation)
+
+    for _ in range(layers):
+        bijectors.append(tfb.MaskedAutoregressiveFlow(
+            shift_and_log_scale_fn=Made(
+                params=2, event_shape=[input_dim], hidden_units=hidden_shape, activation='relu')
+        ))
+
+        bijectors.append(tfb.Permute(permutation=permutation)) #This one one!!!!
+
+    bijector = tfb.Chain(bijectors=list(reversed(bijectors)), name='chain_of_MAF')
+
+    maf = tfd.TransformedDistribution(
+        distribution=tfd.Sample(base_dist, sample_shape=[input_dim]),
+        bijector=bijector,
+    )
+    sys.exit()
+    return maf
+
+
+def create_conditional_flow(
+        hidden_shape: list, layers: int,
+        input_dim: int,
+        conditional_event_shape: tuple):
+    """Create Conditional Masked Autogressive Flow for density estimation
+    Arguments:
+    hidden_shape -- Multilayer Perceptron shape
+    input_dim -- dimensions to be generated
+    layers -- Number of bijectors
+    conditional_event_shape -- dimentionality of conditions
     """
-    base_lr = lr
-    end_lr = 1e-5
-    learning_rate_fn = tfk.optimizers.schedules.PolynomialDecay(
-        base_lr, max_epochs, end_lr, power=0.5)
 
-    # initialize checkpoints
-    checkpoint_directory = "{}/checkpoints".format(outdir)
-    os.makedirs(checkpoint_directory, exist_ok=True)
+    base_dist = tfd.Normal(loc=0.0, scale=1.0)
+    bijectors = []
+    permutation = tf.cast(np.concatenate((
+        np.arange(input_dim // 2, input_dim), np.arange(0, input_dim // 2))), tf.int32)
 
-    opt = tf.keras.optimizers.Adam(learning_rate=learning_rate_fn)  # optimizer
-    checkpoint = tf.train.Checkpoint(optimizer=opt, model=flow_model)
-    ckpt_manager = tf.train.CheckpointManager(checkpoint, checkpoint_directory, max_to_keep=None)
-    _ = checkpoint.restore(ckpt_manager.latest_checkpoint).expect_partial()
+    for i in range(layers):
+        bijectors.append(tfb.MaskedAutoregressiveFlow(
+            shift_and_log_scale_fn=Made(
+                params=2,
+                event_shape=[input_dim],
+                conditional=True,
+                conditional_event_shape=conditional_event_shape,
+                hidden_units=hidden_shape, activation='relu'),
+            name=f"b{i}"
+        ))
+        if input_dim > 1:
+            bijectors.append(tfb.Permute(permutation=permutation))
 
+    bijector = tfb.Chain(
+        bijectors=list(reversed(bijectors)), name='chain_of_MAF')
 
-    AUTO = tf.data.experimental.AUTOTUNE
-    training_data = tf.data.Dataset.from_tensor_slices(
-        train_truth).batch(batch_size).prefetch(AUTO)
+    maf = tfd.TransformedDistribution(
+        distribution=tfd.Sample(base_dist, sample_shape=[input_dim]),
+        bijector=bijector,
+    )
 
-    img_dir = os.path.join(outdir, "imgs")
+    return maf
