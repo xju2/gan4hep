4e90b8dc8e892eea68e65b052155f2b4bc80619f
PeterFitzhugh <92994722+PeterFitzhugh@users.noreply.github.com>
2022-09-23 10:49:50 +0200

    Update atlas_plots.py

--- gan4hep/nf/atlas_plots.py
+++ gan4hep/nf/atlas_plots.py
@@ -12,32 +12,6 @@ import seaborn as sns
 
 import atlasplots as aplt
 
-# 2.7 for phi cuts
-
-def selection_cuts(predictions, truths):
-    predictions_cut = []
-    predictions_cut = predictions[(-1 * 2.7 < predictions[:, 1]) & (predictions[:, 1] < 1 * 2.7)]
-    predictions_cut = predictions_cut[(-1 * np.pi < predictions_cut[:, 2]) & (predictions_cut[:, 2] < 1 * np.pi)]
-    predictions_cut = predictions_cut[(-1 * 2.7 < predictions_cut[:, 4]) & (predictions_cut[:, 4] < 1 * 2.7)]
-    predictions_cut = predictions_cut[(-1 * np.pi < predictions_cut[:, 5]) & (predictions_cut[:, 5] < 1 * np.pi)]
-
-    print('Length of predicted data set post-cuts: ', len(predictions_cut))
-    print('Fraction of events lost : ', 1 - len(predictions_cut) / len(predictions))
-    select_cut_val = 1 - len(predictions_cut) / len(predictions)
-
-    # predictions=np.c_[ predictions ]
-    truths_cut = truths[(-2.7 < truths[:, 1]) & (truths[:, 1] < 2.7)]
-    truths_cut = truths_cut[(-np.pi < truths_cut[:, 2]) & (truths_cut[:, 2] < np.pi)]
-    truths_cut = truths_cut[(-2.7 < truths_cut[:, 4]) & (truths_cut[:, 4] < 2.7)]
-    truths_cut = truths_cut[(-np.pi < truths_cut[:, 5]) & (truths_cut[:, 5] < np.pi)]
-
-    print('Length of true data set post-cuts: ', len(truths_cut))
-    print('Fraction of events lost : ', 1 - len(truths_cut) / len(truths))
-    select_cut_val_truths = 1 - len(truths_cut) / len(truths)
-
-    return predictions_cut, truths_cut, predictions, truths, select_cut_val
-
-
 
 
 
@@ -55,108 +29,6 @@ def end_of_run_plots(w_list,loss_list):
     plt.xlabel('Epoch')
     plt.savefig(os.path.join('Plots', 'wasserstein.png'))
 
-
-def dimuon_calc(truths,jetnum):
-    # print(truths)
-    # Define muon mass and create two arrays filled with this value
-
-    m_u = 1.883531627e-28
-
-    masses_lead = np.full((len(truths[:, 0]), 1), m_u)
-    masses_sub = np.full((len(truths[:, 0]), 1), m_u)
-
-    # Take each column from the tru and generated data and rename to their parameter type
-    pts_lead_true = np.array(truths[:, 0]).flatten()
-    etas_lead_true = np.array(truths[:, 1]).flatten()
-    phis_lead_true = np.array(truths[:, 2]).flatten()
-    pts_sub_true = np.array(truths[:, 3]).flatten()
-    etas_sub_true = np.array(truths[:, 4]).flatten()
-    phis_sub_true = np.array(truths[:, 5]).flatten()
-
-    if jetnum==1:
-        jet1_pt_true=np.array(truths[:, 6]).flatten()
-        jet1_eta_true =np.array(truths[:, 7]).flatten()
-        jet1_phi_true =np.array(truths[:, 8]).flatten()
-
-    # Create lists for 4 vector values
-    muon_lead_true = []
-    muon_sub_true = []
-    parent_true = []
-    jet1_true=[]
-
-    # Create lists for invarient mass values
-    mass_true = []
-    pt_comb_true = []
-    pseudo_true = []
-    eta_angle_btwn_true = []
-    phi_angle_btwn_true = []
-    cos_theta_true = []
-    phi_seperation_true = []
-
-
-
-    for i in range(len(truths)):
-        # Use pylorentz to define 4 momentum arrays for each event
-        muon_lead_true.append(
-            Momentum4.m_eta_phi_pt(masses_lead[i], etas_lead_true[i], phis_lead_true[i], pts_lead_true[i]))
-        muon_sub_true.append(Momentum4.m_eta_phi_pt(masses_sub[i], etas_sub_true[i], phis_sub_true[i], pts_sub_true[i]))
-
-        if jetnum == 1:
-            #WARNING DONT KNOW MASS OF JETT SO PLACEHOLDER AT THE MOMENT
-            # Calculate Jet1 4 Vector
-            jet1_true.append(Momentum4.m_eta_phi_pt(masses_sub[i], jet1_eta_true[i], jet1_phi_true[i], jet1_pt_true[i]))
-
-        # Calculate the Higgs boson 4 vector
-        parent_true.append(muon_lead_true[i] + muon_sub_true[i])
-
-        # Retrieve the Higgs Mass
-        mass_true.append(parent_true[i].m)
-
-        # Retrive PT
-        pt_comb_true.append(parent_true[i].p_t)
-
-        # Retrieve Pseudorapidity
-        pseudo_true.append(parent_true[i].eta)
-
-        # Retrieve eta between the muons
-        eta_angle_btwn_true.append(muon_lead_true[i].eta - muon_sub_true[i].eta)
-
-        # Retrieve phi between the muons
-        phi_angle_btwn_true.append(muon_lead_true[i].phi - muon_sub_true[i].phi)
-        if jetnum == 1:
-        #azimuthal seperation between muons and jets
-            phi_seperation_true.append((muon_lead_true[i].phi - muon_sub_true[i].phi) - jet1_true[i].phi)
-
-        # Calculate P12+-
-
-        P1_pos = (muon_lead_true[i].e + muon_lead_true[i].p_z) / np.sqrt(2)
-        P1_neg = (muon_lead_true[i].e - muon_lead_true[i].p_z) / np.sqrt(2)
-        P2_pos = (muon_sub_true[i].e + muon_sub_true[i].p_z) / np.sqrt(2)
-        P2_neg = (muon_sub_true[i].e - muon_sub_true[i].p_z) / np.sqrt(2)
-
-        # Calculate Cos
-
-        cos_theta = 2 * (P1_pos * P2_neg - P1_neg * P2_pos)
-        cos_theta = cos_theta / (
-            np.sqrt((parent_true[i].m ** 2) * ((parent_true[i].m ** 2) + (parent_true[i].p_t ** 2))))
-        cos_theta = cos_theta * (parent_true[i].p_z / np.abs(parent_true[i].p_z))
-
-        cos_theta_true.append(cos_theta)
-
-    # Add mass arrays from each batch?
-    mass_true = np.concatenate(mass_true, axis=0)
-
-    # Add to original dataset
-    truths = np.column_stack((truths, mass_true))
-    truths = np.column_stack((truths, pt_comb_true))
-    truths = np.column_stack((truths, pseudo_true))
-    truths = np.column_stack((truths, cos_theta_true))
-    # truths = np.column_stack((truths, eta_angle_btwn_true))
-    truths = np.column_stack((truths, phi_angle_btwn_true))
-    if jetnum==1:
-        truths = np.column_stack((truths, phi_seperation_true))
-
-    return truths
 
 
 def Plotting(truths_cut, predictions_cut, col_num, lower_range, upper_range, title,units):
@@ -244,14 +116,10 @@ def Plotting(truths_cut, predictions_cut, col_num, lower_range, upper_range, tit
 
 def main(truths,predictions,w_list,loss_list,new_run_folder,jetnum,xlabels):
 
-    # Function to calculate invarient dimuon mass + calculated variables
-    truths = dimuon_calc( truths,jetnum)
-    predictions = dimuon_calc(predictions,jetnum)
 
     # Applying Selection Cuts
     print('Length of predicted data set pre-cuts: ', len(predictions))
     print('Length of truth data set pre-cuts: ', len(truths))
-    predictions_cut, truths, predictions, truths_cut, select_cut_val = selection_cuts(predictions, truths)
 
 
     #Original Variables
@@ -417,10 +285,10 @@ def main(truths,predictions,w_list,loss_list,new_run_folder,jetnum,xlabels):
     from IPython.display import IFrame
 
     for i in range(len(var_list)):
-        Plotting(truths_cut, predictions_cut, var_list[i][0], var_list[i][1], var_list[i][2], var_name[i],units)
+        Plotting(truths, predictions, var_list[i][0], var_list[i][1], var_list[i][2], var_name[i],units)
 
     # Check if all elements in array are zero
-    #for i in range(len(predictions_cut[1,:])):
+    #for i in xrange(len(predictions_cut[1,:])):
     #    print((truths_cut[:,i] < 0).any(axis=0))
 
 if __name__ == '__main__':
@@ -433,8 +301,8 @@ if __name__ == '__main__':
 
 
 
-    truths = np.load('Temp_Data/truths.npy')
-    predictions = np.load('Temp_Data/predictions.npy')
+    truths = np.load('Temp_Data/truths_calc.npy')
+    predictions = np.load('Temp_Data/predictions_calc.npy')
     w_list = np.load('Temp_Data/w_list.npy')
     loss_list = np.load('Temp_Data/loss_list.npy')
     f = open("Temp_Data/filename.txt", "r")
